c1e927dc33e902d0d2285afc88780b86f2f0ce79
Twhite1195 <twhite1195@gmail.com>
2020-12-05 13:11:57 -0600

    added AppReservasDW

---
+++ Clase_10_11/AppReservasSW/AppReservasSW/Models/Modelo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppReservasSW.Models
+{
+    public class Modelo
+    {
+        public int MOD_ID { get; set; }
+        public string MOD_MARCA { get; set; }
+        public string MOD_NOMBRE { get; set; }
+    }
+}

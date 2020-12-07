79746fbb57dd02b970c5880d8eda3d5d2d49c8c0
ManuCL <43527050+ManuCL@users.noreply.github.com>
2020-12-06 23:30:42 -0600

    Models
    Reservacion y Sede

---
+++ Clase_10_11/AppReservasSW/AppReservasSW/Models/Sede.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppReservasSW.Models
+{
+    public class Sede
+    {
+        public int SED_ID { get; set; }
+        public string SED_NOMBRE { get; set; }
+        public string SED_UBICACION { get; set; }
+    }
+}

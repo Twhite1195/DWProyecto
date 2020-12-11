650c9e5da24ce4ead7cf4feae66dc0917db827ac
Dean FB <43184649+deanminik@users.noreply.github.com>
2020-12-10 21:54:24 -0600

    Avance aparte

---
+++ Segundo Avance Dean/Controller/PuestoManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AppReservasSW.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.Text;
+
+
+namespace AppReservasSW.Controllers
+{
+    public class PuestoManager
+    {
+        const string URL = "http://localhost:49220/api/lote/";
+        const string URLIngresar = "http://localhost:49220/api/lote/ingresar/";
+
+        HttpClient GetClient(string token)
+        {
+            HttpClient client = new HttpClient();
+
+            client.DefaultRequestHeaders.Add("Authorization", token);
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+            return client;
+        }
+        public async Task<IEnumerable<Puesto>> ObtenerPuestos(string token)
+        {
+            HttpClient client = GetClient(token);
+            string resultado = await client.GetStringAsync(URL);
+
+            return JsonConvert.DeserializeObject<IEnumerable<Puesto>>(resultado);
+        }
+
+        public async Task<IEnumerable<Puesto>> ObtenerLote(string token, string codigo)
+        {
+            HttpClient client = GetClient(token);
+            string resultado = await client.GetStringAsync(URL + codigo);
+            return JsonConvert.DeserializeObject<IEnumerable<Puesto>>(resultado);
+        }
+        public async Task<Puesto> Ingresar(Puesto puesto, string token)
+        {
+            HttpClient client = GetClient(token);
+            var response = await client.PostAsync(URLIngresar,
+                new StringContent(JsonConvert.SerializeObject(puesto), Encoding.UTF8,
+                "application/json"));
+            return JsonConvert.DeserializeObject<Puesto>(await response.Content.ReadAsStringAsync());
+        }
+        public async Task<Puesto> Actualizar(Puesto puesto, string token)
+        {
+            HttpClient client = GetClient(token);
+            var response = await client.PutAsync(URL,
+                new StringContent(JsonConvert.SerializeObject(puesto), Encoding.UTF8,
+                "application/json"));
+            return JsonConvert.DeserializeObject<Puesto>(await response.Content.ReadAsStringAsync());
+        }
+        public async Task<string> Eliminar(string codigo, string token)
+        {
+            HttpClient client = GetClient(token);
+            var response = await client.DeleteAsync(URL + codigo);
+
+            return JsonConvert.DeserializeObject<string>(await response.Content.ReadAsStringAsync());
+        }
+    }
+}
---
+++ Segundo Avance Dean/Views/Empleado.aspx.designer.cs
@@ -0,0 +1,152 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace AppReservasSW.Views
+{
+
+
+    public partial class Empleado
+    {
+
+        /// <summary>
+        /// Control grdEmpleados.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView grdEmpleados;
+
+        /// <summary>
+        /// Control txt_empleado_id.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_empleado_id;
+
+        /// <summary>
+        /// Control txt_nombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_nombre;
+
+        /// <summary>
+        /// Control txt_apellido.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_apellido;
+
+        /// <summary>
+        /// Control txt_telefono.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_telefono;
+
+        /// <summary>
+        /// Control txt_cedula.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_cedula;
+
+        /// <summary>
+        /// Control txt_residencia.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_residencia;
+
+        /// <summary>
+        /// Control Label2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+
+        /// <summary>
+        /// Control drpSede.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList drpSede;
+
+        /// <summary>
+        /// Control Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// Control drpPuesto.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList drpPuesto;
+
+        /// <summary>
+        /// Control btnAgregar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAgregar;
+
+        /// <summary>
+        /// Control btnModificar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnModificar;
+
+        /// <summary>
+        /// Control btnEliminar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnEliminar;
+
+        /// <summary>
+        /// Control lblStatus.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblStatus;
+    }
+}
